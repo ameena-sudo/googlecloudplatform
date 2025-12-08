bbc2e8fd027d294f11923bb10bc9935e937ccdf6
cloudengineer <ameenaasma35@gmail.com>
2025-12-08 18:59:58 +0300

    Add missing newline at end of blue.sh

--- LoadBalancer/NetworkLB/blue/blue.sh
+++ LoadBalancer/NetworkLB/blue/blue.sh
@@ -5,3 +5,4 @@ cd /var/www/html
 rm -rf index.html
 wget https://raw.githubusercontent.com/devopswithcloud/GoogleCloudPlatform/master/LoadBalancer/NetworkLB/blue/index.html
 sudo service apache2 restart
+.....
